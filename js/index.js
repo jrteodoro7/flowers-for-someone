9bf14e0ad6c90477c1d053569bb856cd972c7b18
jrteodoro7 <jrteodoro2008@gmail.com>
2024-12-28 01:12:21 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'Eu tenho uma coisa para te dizer'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
