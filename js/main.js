bb36fdd42f85614bdccd8757f05ff69c9a4c3b88
jrteodoro7 <jrteodoro2008@gmail.com>
2024-12-28 01:12:14 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE YOU ISA!').split('')
+    const titles = ('I LOVE YOU CACHINHOS DOURADOS!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
