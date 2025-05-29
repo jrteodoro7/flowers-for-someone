80cce9a5b8951b3cf087c8e4b9810d3cbe5dd57d
jrteodoro7 <jrteodoro2008@gmail.com>
2025-05-29 12:55:17 -0300

    29/05
    29/05

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE YOU CACHINHOS DOURADOS!').split('')
+    const titles = ('YOU ARE THE LOVE OF MY LIFE, I LOVE YOU MINHA JOGADORA!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
